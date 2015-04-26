ca97bc63198b2fec906dfb083a0b86e06beaab32
Simon <svansintjan@gmail.com>
2015-04-26 08:25:23 +0200

    Tagging updates

--- auto.rb
+++ auto.rb
@@ -18,18 +18,22 @@ list_id = list['data'][0]['id']
 pinboard = Pinboard::Client.new(:username => ENV['PINBOARD_USERNAME'],
                                 :password => ENV['PINBOARD_PASSWORD'])
 
-edition = 117
+edition = 118
 
 upcoming = pinboard.posts(tag: 'uxweekly-sending')
 
 upcoming_hash = upcoming.map do |link|
+  tag_regex = /\/\/\/([A-Za-z]*)\/\/\//
+  tag = tag_regex.match(link.to_h[:extended])
+
   # Liquid only accepts string hashes, not ruby 1.9 hashes
   # https://github.com/Shopify/liquid/issues/289
   link_hash = link.to_h
   { "description" => link_hash[:description],
     "href" => link_hash[:href],
-    "extended" => link_hash[:extended],
-    "tags" => link_hash[:tags] }
+    "extended" => link_hash[:extended].gsub(tag_regex, ''),
+    "tag" => tag ? tag[1] : '' }
+
 end
 
 subject = "UX Weekly #{edition}: Not just another link dump"
@@ -38,6 +42,8 @@ source = File.read('templates/uxweekly-mailchimp-template.liquid', :encoding => 
 html = Liquid::Template.parse(source).render 'upcoming' => upcoming_hash,
                                              'edition' => edition,
                                              'sent_on' => Date.today
+
+# puts html
 
 gb.campaigns.create({ type: 'regular',
                       options: { list_id: list_id,
